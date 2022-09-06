9a7d01f015b8d99ac3ddb47d545dd8c984fa4f7c
Trance-0 <60459821+Trance-0@users.noreply.github.com>
2022-09-06 10:40:51 -0500

    Create L814_1.java

---
+++ Leetcode/src/medium/L814_1.java
@@ -0,0 +1,36 @@
+public class L814_1 {
+    /**
+     * Definition for a binary tree node.
+     */
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public TreeNode pruneTree(TreeNode root) {
+
+        if (root.left != null)
+            root.left = pruneTree(root.left);
+        if (root.right != null)
+            root.right = pruneTree(root.right);
+        if (root.left == null && root.right == null && root.val == 0)
+            return null;
+        else
+            return root;
+    }
+}

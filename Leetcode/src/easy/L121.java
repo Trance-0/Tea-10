c3fa1ab1ff1ab3d2f8359da3b71bff07682344b2
Trance-0 <60459821+Trance-0@users.noreply.github.com>
2022-12-08 16:07:28 -0600

    SHIT

---
+++ Leetcode/src/easy/L121.java
@@ -0,0 +1,12 @@
+ package easy;
+ class L121{
+ public int maxProfit(int[] prices) {
+        int smallest_before_i=prices[0];
+        int highest_profit=0;
+        for(int i=1;i<prices.length;i++){
+            highest_profit=Math.max(highest_profit,prices[i]-smallest_before_i);
+            smallest_before_i=Math.min(smallest_before_i,prices[i]);
+        }
+        return highest_profit;
+    }
+}s
---
+++ Leetcode/src/easy/L892.java
@@ -0,0 +1,47 @@
+package easy;
+
+import java.util.LinkedList;
+
+public class L892 {
+  
+//  Definition for a binary tree node.
+ public class TreeNode {
+    int val;
+  TreeNode left;
+     TreeNode right;
+     TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+     TreeNode(int val, TreeNode left, TreeNode right) {
+         this.val = val;
+         this.left = left;
+        this.right = right;
+    }
+ }
+
+    public boolean leafSimilar(TreeNode root1, TreeNode root2) {
+        LinkedList<Integer> list1=new LinkedList<Integer>();        
+        LinkedList<Integer> list2=new LinkedList<Integer>();
+        dfsLeaf(list1,root1);
+        dfsLeaf(list2,root2);
+        if (list1.size()!=list2.size()){
+            return false;
+            }
+for(int i=0;i<list1.size();i++){
+if(list1.get(i)!=list2.get(i)){
+return false;
+}
+}
+return true;
+    }
+    private void dfsLeaf(LinkedList<Integer>val,TreeNode test){
+        if(test==null){
+        return;
+        }
+        if(test.left==null&&test.right==null){
+            val.add(test.val);
+            return;
+        }
+        dfsLeaf(val,test.right);
+        dfsLeaf(val,test.left);
+    }
+}

2eea5f978d5f9acc8582b98463d23e3e610fdd2f
Trance-0 <60459821+Trance-0@users.noreply.github.com>
2023-01-01 00:33:18 -0800

    test in visual studio

--- Leetcode/src/medium/L797.cpp
+++ Leetcode/src/medium/L797.cpp
@@ -1,6 +1,43 @@
+#include <bits/stdc++.h>;
+using namespace std;
 class Solution {
 public:
     vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
-        
+        int n = 0;
+        n = graph.size();
+        vector<vector<int>> result;
+        // I will use dfs
+        vector<int> depth;
+        vector<bool> arr;
+        for (int i = 0; i < n; i++) {
+            arr.push_back(false);
+        }
+        arr[0] = true;
+        depth.push_back(0);
+        dfs(n - 1, arr, depth, graph, result);
+        return result;
+    }
+    void dfs(int end, vector<bool>& hist, vector<int>& depth,
+        vector<vector<int>>& graph, vector<vector<int>> result) {
+        // the base case;
+        if (depth.back() == end) {
+            ;
+            result.push_back(depth);
+            return;
+        }
+        for (int i : graph[depth.back()]) {
+            if (!hist[i]) {
+                hist[i] = true;
+                depth.push_back(i);
+                dfs(end, hist, depth, graph, result);
+                depth.pop_back();
+                hist[i] = false;
+            }
+        }
     }
 };
+int main() {
+    Solution a;
+    vector<vector<int>> graph;
+    std::cout << allPathsSourceTarget(graph);
+}
